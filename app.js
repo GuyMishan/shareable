492d91ee844aa15ef1ca677fa8d346b5c11477a7
GuyMishan <gaiu01999@gmail.com>
2021-12-26 13:59:48 +0200

    first commit

--- app.js
+++ app.js
@@ -103,9 +103,9 @@ if (process.env.NODE_ENV === "production") {
   app.use(logger("dev"));
 }
 app.use(express.static("public"));
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "public", "index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "public", "index.html"));
+// });
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
