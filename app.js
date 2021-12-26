ac3a4b0daf1aaa5947fcba2b3148ee3fddfad6b6
GuyMishan <gaiu01999@gmail.com>
2021-12-26 13:41:06 +0200

    first commit

--- app.js
+++ app.js
@@ -140,4 +140,19 @@ app.use((err, req, res, next) => {
   });
 });
 
+if(process.env.NODE_ENV === 'production'){
+    //set static folder
+    app.use(express.static('client/build'));
+    app.get('*', (req,res)=>{
+      res.sendFile(path.resolve(__dirname,'client', 'build', 'index.html'));
+    });  
+  }
+
+  //listen
+  const port = process.env.PORT || 8000;
+  app.listen(port, () => {
+      console.log(`Server is running on port ${port}`)
+  });
+  
+
 module.exports = app;
