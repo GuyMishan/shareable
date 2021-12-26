6cc90068514884e1d12ae6f4817e3b04e0ffe24b
GuyMishan <gaiu01999@gmail.com>
2021-12-26 14:43:43 +0200

    first commit

--- app.js
+++ app.js
@@ -141,7 +141,7 @@ const publicPath = path.join(__dirname, '.', 'public');
 
 app.use(express.static('client/build'));
 app.get('*', (req,res)=>{
-  res.sendFile(path.resolve(__dirname,'client', 'build', 'index.html'));
+  res.sendFile(path.resolve(__dirname,'client', 'public', 'index.html'));
 });  
 
 module.exports = app;
